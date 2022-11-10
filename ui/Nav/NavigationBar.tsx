abb0ab4b18dddc87eeb554d56c034f31ba066a99
Kwesi Hervie <herviek2001@gmail.com>
2022-11-10 21:43:37 +0100

    Added mix blend mode

--- ui/Nav/NavigationBar.tsx
+++ ui/Nav/NavigationBar.tsx
@@ -11,11 +11,11 @@ const NavigationBar = () => {
   const [isOpen, toggleOpen] = useCycle(false, true);
 
   return (
-    <div className="w-full h-[70px] box-border fixed left-0 right-0 px-[1em] md:px-[3em] py-[1em] flex items-center justify-between">
-      <div>
+    <div className="w-full h-[70px] box-border fixed left-0 right-0 px-[1em] md:px-[3em] py-[1em] flex items-center justify-between z-[9999] mix-blend-difference ">
+      <div className="">
         <Link
           href="/"
-          className="font-medium text-[20px] md:text-[32px] mx-4 my-12 px-2 py-12 cursor-pointer text-[#171717] dark:text-white"
+          className="font-medium text-[20px] md:text-[32px] mx-4 my-12 px-2 py-12 cursor-pointer text-[#171717] dark:text-white "
         >
           Khervie00
         </Link>
