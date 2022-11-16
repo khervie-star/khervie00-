b692386c46c7cc7d6693d0d9ec9fe790b730fe07
Kwesi Hervie <herviek2001@gmail.com>
2022-11-16 12:50:16 +0100

    Started with ogimages and favicons

--- app/head.tsx
+++ app/head.tsx
@@ -4,12 +4,27 @@ export default function Head() {
     <>
       <title>Kwesi Hervie</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta
-        name="description"
-        content="Hi, Im Kwesi Hervie. And here is my portfolio website"
+      <meta name="description" content="Kwesi Hervie | Frontend Dev" />
+      <link rel="icon" href="/favicon/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
       />
-      <link rel="icon" href="/favicon.ico" />
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/static/favicon/site.webmanifest" />
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
